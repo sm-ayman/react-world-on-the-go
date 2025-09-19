124b3d00743cea6c88ff671db807b6c20831c5a7
sm_ayman <sultanmdayman@gmail.com>
2025-09-20 01:32:42 +0600

    countries suspense created

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { use } from "react";
 
-const Countries = () => {
-    return (
-        <div>
-            <h1>From Countries...</h1>
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  console.log(countries);
+  return (
+    <div>
+      <h1>From Countries...{countries.length}</h1>
+    </div>
+  );
 };
 
 export default Countries;
