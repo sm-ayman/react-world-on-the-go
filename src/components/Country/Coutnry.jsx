e34723c04658bc8fcb63f845f36177d6774a27d7
sm_ayman <sultanmdayman@gmail.com>
2025-09-21 00:56:18 +0600

    visited button added

--- src/components/Country/Coutnry.jsx
+++ src/components/Country/Coutnry.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Coutnry = ({ country }) => {
-  console.log(country);
+  const [visited, setVisited] = useState(false);
+  // console.log(country);
+
+  const handleVisit = () => {
+    // system-1
+    /* if (visited) {
+      setVisited(false);
+      alert(`${country.name.common} set to Not Visited`);
+    } else {
+      setVisited(true);
+      alert(`${country.name.common} Visited`);
+    } */
+
+  //  system-2
+    setVisited(!visited);
+    alert(
+      `${country.name.common} ${visited ? "set to Not Visited" : "Visited"}`
+    );
+  };
+
   return (
     <div className="bg-white shadow-lg rounded-2xl overflow-hidden p-4 hover:shadow-2xl transition duration-300">
       <img
@@ -24,6 +43,18 @@ const Coutnry = ({ country }) => {
           {country.continents.continents}
         </span>
       </p>
+      <p className="text-gray-600 mt-2 text-sm">
+        <span className="font-medium text-gray-800">Area:</span>{" "}
+        {country.area.area}
+      </p>
+      <div className="text-end mt-2">
+        <button
+          onClick={handleVisit}
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+        >
+          {visited ? "Visited" : "Not Visited"}
+        </button>
+      </div>
     </div>
   );
 };
